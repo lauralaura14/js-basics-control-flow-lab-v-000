3c2fd22a3d5c9a9129753837f030081481dc3abe
Laura <laurao148@gmail.com>
2018-07-30 00:41:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code in this file!
+function scuberGreetingForFeet (someValue) {
+  let result;
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  }
+  else if (someValue > 2000 && someValue < 2500){
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500) {
+    result = "No can do.";
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  if (city == "NYC") {
+    result = "Ok, sounds good.";
+  }
+  else if ()
+  return result;
+}
