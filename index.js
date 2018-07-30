536c94be8a64b6bbe55e47fc00f229072a0f6748
Laura <laurao148@gmail.com>
2018-07-30 00:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function scuberGreetingForFeet(someValue) {
+function scuberGreetingForFeet (someValue) {
   let result;
   if (someValue <= 400) {
     result = "This one is on me!";
   }
+  else if (someValue > 2000 && someValue < 2500){
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500) {
+    result = "No can do.";
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  let result;
+  if (city == "NYC") {
+    result = "Ok, sounds good.";
+  }
+  else if ()
   return result;
 }
